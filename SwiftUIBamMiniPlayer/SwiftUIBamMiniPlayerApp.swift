71c12ae63de3905dd350d1b076f539509e78706f
Studio Kaori <studiokaori.com@gmail.com>
2022-08-30 17:24:48 +0200

    Initial Commit

---
+++ SwiftUIBamMiniPlayer/SwiftUIBamMiniPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBamMiniPlayerApp.swift
+//  SwiftUIBamMiniPlayer
+//
+//  Created by Kaori Persson on 2022-08-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBamMiniPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
