060ccb4d527070d30738ba9a91016a75786e42f8
Studio Kaori <studiokaori.com@gmail.com>
2022-09-01 22:11:45 +0200

    Add: ObservablePlayerState

---
+++ SwiftUIBamMiniPlayer/EnvironmentObject/ObservablePlayerState.swift
@@ -0,0 +1,13 @@
+//
+//  ObservedPlayerState.swift
+//  SwiftUIBamMiniPlayer
+//
+//  Created by Kaori Persson on 2022-09-01.
+//
+
+import SwiftUI
+
+class ObservablePlayerState: ObservableObject {
+    @Published var isPlayerViewVisible = false
+    
+}
